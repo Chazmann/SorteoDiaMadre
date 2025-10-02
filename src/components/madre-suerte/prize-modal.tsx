dea2a45068284792efcdbbe6fa64d1a9c016d29e
Leandro Chazarreta <lchaz85@gmail.com>
2025-10-02 22:04:11 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/madre-suerte/prize-modal.tsx
+++ src/components/madre-suerte/prize-modal.tsx
@@ -19,6 +19,12 @@ export function PrizeModal({ isOpen, onOpenChange }: PrizeModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-4xl">
+         <DialogHeader>
+            <DialogTitle>Premios del Sorteo</DialogTitle>
+            <DialogDescription>
+                Una lista de los premios disponibles para el sorteo del Día de la Madre.
+            </DialogDescription>
+        </DialogHeader>
          <div className="max-h-[80vh] overflow-y-auto p-4">
             <PrizeList />
          </div>
