4f2f65967de65cd72587941ec57294800273deab
Leandro Chazarreta <lchaz85@gmail.com>
2025-10-02 16:49:42 +0000

    actualizo nombre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default function Home() {
           <div className="inline-flex items-center gap-3 mb-2">
             <HeartIcon className="w-8 h-8 text-primary" />
             <h1 className="text-4xl md:text-5xl font-headline font-bold tracking-tight text-primary-foreground bg-primary px-4 py-2 rounded-lg shadow-lg">
-              Madre Suerte - 7mo 1ra
+              Sorteo día de la Madre - 7mo 1ra
             </h1>
             <HeartIcon className="w-8 h-8 text-primary" />
           </div>
