e8cb75c75ff2ded5b469672ec0f2ef10e518b7ce
Leandro Chazarreta <lchaz85@gmail.com>
2025-10-03 00:13:43 +0000

    generar un valor para --accent acorde al nuevo valor ingresado en --prim

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -77,7 +77,6 @@ export default function LoginPage() {
       if (seller) {
         localStorage.setItem('loggedInSeller', JSON.stringify(seller));
         toast({
-          variant: 'success',
           title: '¡Bienvenido/a!',
           description: `Has iniciado sesión como ${seller.name}.`,
         });
